9fd4d30c3399c8e07a621429d97249709da319e5
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-12-29 21:27:14 +0530

    added routes to the Login component

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,12 +1,18 @@
-
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.scss";
 
 function App() {
   
 
   return (
-    <>
-      <div>Chat</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
