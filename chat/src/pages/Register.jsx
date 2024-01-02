e8c3415413671675cd29a35e16203b776b470e2c
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-01-02 19:17:52 +0530

    firebase email auth

--- chat/src/pages/Register.jsx
+++ chat/src/pages/Register.jsx
@@ -1,28 +1,53 @@
 // registration form page
 import "../index.scss";
 import Img from "../assets/icons8-image-144.png";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-function Register(){
-    return(
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">Chatify</span>
-                <span className="title">Register</span>
-                <form>
-                    <input type="text" placeholder="Name"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="file" id="file" style={{display: "none"}} />
-                    <label htmlFor="file">
-                        <img src={Img} alt="image"/>
-                        <span>Add an avatar</span>
-                    </label>
-                    <button>Sign Up</button>
-                </form>
-                <p>Do you have an account? Login</p>
-            </div>
-        </div>
-    )
+function Register() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+        console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // ..
+    });
+    
+  }
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chatify</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="file" id="file" style={{ display: "none" }} />
+          <label htmlFor="file">
+            <img src={Img} alt="image" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>Do you have an account? Login</p>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
