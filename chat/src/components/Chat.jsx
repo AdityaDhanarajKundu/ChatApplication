75b8ade715d906f900b030c51890ebbd8b11141f
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-01-02 15:21:25 +0530

    designed the input of the chat section

--- chat/src/components/Chat.jsx
+++ chat/src/components/Chat.jsx
@@ -1,7 +1,23 @@
+import Cam from "../assets/cam.png";
+import Add from "../assets/add.png";
+import More from "../assets/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Alicia</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="cam" />
+          <img src={Add} alt="add" />
+          <img src={More} alt="more" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
   );
 }
 
---
+++ chat/src/components/Messages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Messages = () => {
+  return (
+    <div className='messages'>
+        <p>Messages</p>
+        <p>Messages</p>
+        <p>Messages</p>
+        <p>Messages</p>
+    </div>
+  );
+}
+
+export default Messages;
