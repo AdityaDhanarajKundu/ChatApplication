c9ef656999d52cb0eb4fbd66543e1b9f80a2dd3d
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-01-03 23:18:51 +0530

    dynamically showing the messages real time

--- chat/src/components/Message.jsx
+++ chat/src/components/Message.jsx
@@ -1,13 +1,31 @@
+import { useContext,useRef, useEffect } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../context/AuthContext";
+
 const Message = ({message}) => {
+
+  const ref = useRef();
+
+  const {data} = useContext(ChatContext);
+  const {currentUser} = useContext(AuthContext);
+
+  useEffect(()=>{
+    ref.current.scrollIntoView({behavior: "smooth"}); // scroll to the current latest message every time there is new message
+  },[message]);
+  
   return (
-    <div className="message owner">
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
       <div className="messageInfo">
-        <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" />
+        <img src={
+          message.senderId === currentUser.uid
+          ? currentUser.photoURL
+          : data.user.photoURL
+        } alt="image" />
         <span>Just Now</span>
       </div>
       <div className="messageContent">
-        <p>Hello</p>
-        <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" />
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="image" />}
       </div>
     </div>
   );
