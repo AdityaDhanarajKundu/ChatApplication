612b12d11e7cc451339d77641c07604f8368e2a2
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-01-03 12:26:24 +0530

    firebase storage and database collections to upload files

--- chat/src/pages/Register.jsx
+++ chat/src/pages/Register.jsx
@@ -1,31 +1,63 @@
 // registration form page
 import "../index.scss";
 import Img from "../assets/icons8-image-144.png";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
-  function handleSubmit(e) {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    const name = e.target[0].value;
+    const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed up
-      const user = userCredential.user;
-        console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-      // ..
-    });
-    
+    try {
+        //Create user
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+  
+        //Create a unique image name
+        const date = new Date().getTime();
+        const storageRef = ref(storage, `${displayName + date}`);
+  
+        await uploadBytesResumable(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then(async (downloadURL) => {
+            try {
+              //Update profile
+              await updateProfile(res.user, {
+                displayName,
+                photoURL: downloadURL,
+              });
+              //create user on firestore
+              await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
+                displayName,
+                email,
+                photoURL: downloadURL,
+              });
+  
+              //create empty user chats on firestore
+              await setDoc(doc(db, "userChats", res.user.uid), {});
+              navigate("/");
+            } catch (err) {
+              console.log(err);
+              setError(true);
+              setLoading(false);
+            }
+          });
+        });
+      } catch (err) {
+        setError(true);
+        setLoading(false);
+      }
   }
 
   return (
@@ -43,6 +75,7 @@ function Register() {
             <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
+          {error && <span className="error">Something went wrong!</span>}
         </form>
         <p>Do you have an account? Login</p>
       </div>
