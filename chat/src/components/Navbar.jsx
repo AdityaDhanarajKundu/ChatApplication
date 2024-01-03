c14fb1b3e13607d052e1f2710e810672cb6b8f57
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-01-03 13:36:39 +0530

    fetching the displayname and the photo from the current user for the navbar

--- chat/src/components/Navbar.jsx
+++ chat/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+  
   return (
     <div className="navbar">
       <span className="logo">Chatify</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile" />
-        <span>Alicia</span>
+        <img src={currentUser.photoURL} alt="profile" />
+        <span>{currentUser.displayName}</span>
         <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
